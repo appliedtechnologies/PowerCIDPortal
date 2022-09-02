b0ba029f470c487cdf80884e789afce8a671c1be
Fabian Giesche <FGiesche@apptech.de>
2022-09-02 12:28:34 +0200

    add connections owner

--- at.D365.PowerCID.Portal/Controllers/EnvironmentsController.cs
+++ at.D365.PowerCID.Portal/Controllers/EnvironmentsController.cs
@@ -39,7 +39,7 @@ namespace at.D365.PowerCID.Portal.Controllers
             if((await this.dbContext.Environments.FirstOrDefaultAsync(e => e.Id == key && e.TenantNavigation.MsId == this.msIdTenantCurrentUser)) == null)
                 return Forbid();
 
-            string[] propertyNamesAllowedToChange = { "OrdinalNumber", "IsDevelopmentEnvironment" };
+            string[] propertyNamesAllowedToChange = { "OrdinalNumber", "IsDevelopmentEnvironment", "ConnectionsOwner" };
             if (environment.GetChangedPropertyNames().Except(propertyNamesAllowedToChange).Count() == 0)
             {
                 if (!ModelState.IsValid)
