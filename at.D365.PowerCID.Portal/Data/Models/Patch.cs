608337f19db5d7263998ce15855b377485a0e2d1
Fabian Giesche <FGiesche@apptech.de>
2023-03-21 16:58:06 +0100

    add Controller Method

--- at.D365.PowerCID.Portal/Data/Models/Patch.cs
+++ at.D365.PowerCID.Portal/Data/Models/Patch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 #nullable disable
 
@@ -8,5 +9,11 @@ namespace at.D365.PowerCID.Portal.Data.Models
     public partial class Patch : Solution
     {
         public bool WasDeleted { get; set; }
+
+        public bool IsDeletable { get {
+            bool hasSuccessfulImports = this.Actions.Where(e => e.Type == 2 && e.Result == 1).Count() > 0;
+            bool hasRunningActions = this.Actions.Where(e => e.Status == 1 || e.Status == 2).Count() > 0;
+            return !hasSuccessfulImports && !hasRunningActions;
+        } } 
     }
 }
