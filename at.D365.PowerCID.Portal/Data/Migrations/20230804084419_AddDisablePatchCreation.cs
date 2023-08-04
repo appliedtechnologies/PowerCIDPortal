6ef19d124a6b8a42c0b7cf28cad9eb4ceccb5424
Fabian Giesche <FGiesche@apptech.de>
2023-08-04 10:45:43 +0200

    extend model

---
+++ at.D365.PowerCID.Portal/Data/Migrations/20230804084419_AddDisablePatchCreation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace at.D365.PowerCID.Portal.Data.Migrations
+{
+    public partial class AddDisablePatchCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DisablePatchCreation",
+                table: "Tenant",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisablePatchCreation",
+                table: "Tenant");
+        }
+    }
+}
