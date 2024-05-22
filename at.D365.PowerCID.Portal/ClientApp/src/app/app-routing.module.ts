784e7233a84853376433dc6d2add4de42b3b062b
Fabian Giesche <FGiesche@apptech.de>
2024-05-22 21:36:47 +0200

    update to angular v15

--- at.D365.PowerCID.Portal/ClientApp/src/app/app-routing.module.ts
+++ at.D365.PowerCID.Portal/ClientApp/src/app/app-routing.module.ts
@@ -84,7 +84,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
