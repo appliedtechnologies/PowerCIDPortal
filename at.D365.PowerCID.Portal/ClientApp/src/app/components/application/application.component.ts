9670d28fa9670b10fef2c2e9dce16a8482180fdb
Fabian Giesche <FGiesche@apptech.de>
2024-08-20 10:36:40 +0200

    take ForceManagedDeplyoment into account during deployment

--- at.D365.PowerCID.Portal/ClientApp/src/app/components/application/application.component.ts
+++ at.D365.PowerCID.Portal/ClientApp/src/app/components/application/application.component.ts
@@ -298,6 +298,7 @@ export class ApplicationComponent {
   onInitNewRowApplication(e) {
     this.publisherSelectionDisabled = true;
     this.filteredPublishers = [];
+    e.data.ForceManagedDeployment = false;
   }
 
   onValueChangedEnvironment(event, cellInfo) {
